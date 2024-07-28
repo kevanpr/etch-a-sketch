643d4601fd8433064ce30194468eb68f390ab1cb
kevanpr <gracellepontanilla@gmail.com>
2024-07-28 23:49:09 +0800

    fix: put 16 columns inside rows and iterate process 16 times

--- javascript.js
+++ javascript.js
@@ -2,9 +2,16 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     let sketch = document.querySelector(".sketch");
 
-    for (let i = 1; i <= 16*16; i++) {
-        let pixel = document.createElement("div");
-        pixel.setAttribute("class", "pixel");
-        sketch.appendChild(pixel);
+    for (let j = 1; j <= 16; j++) {
+        let row = document.createElement("div");
+        row.setAttribute("class", "row");
+        sketch.appendChild(row);
+    
+        for (let i = 1; i <= 16; i++) {
+            let column = document.createElement("div");
+            column.setAttribute("class", "column");
+            row.appendChild(column);
+        };
     };
+    
 });
