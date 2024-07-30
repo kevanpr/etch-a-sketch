ce862e34a7387f6614600a6e9efa1c58f0f3d7e9
kevanpr <gracellepontanilla@gmail.com>
2024-07-30 20:13:26 +0800

    fix: add createGrid and pixelSize function and event listener to slider

--- javascript.js
+++ javascript.js
@@ -1,23 +1,74 @@
 
 window.addEventListener("DOMContentLoaded", (event) => {
     let sketch = document.querySelector(".sketch");
-
-    for (let j = 1; j <= 16; j++) {
-        let row = document.createElement("div");
-        row.setAttribute("class", "row");
-        sketch.appendChild(row);
-    
-        for (let i = 1; i <= 16; i++) {
-            let column = document.createElement("div");
-            column.setAttribute("class", "column");
-            row.appendChild(column);
-        };
-    };
-
     let slider = document.querySelector(".slider");
     let sliderValue = document.querySelector(".sliderValue");
+
+    function createGrid (gridNumber) {
+        for (let j = 1; j <= gridNumber; j++) {
+            let row = document.createElement("div");
+            row.setAttribute("class", "row");
+            sketch.appendChild(row);
+        
+            for (let i = 1; i <= gridNumber; i++) {
+                let column = document.createElement("div");
+                column.setAttribute("class", "column");
+                row.appendChild(column);
+            }
+        }
+    }
+
+    function pixelSize() {
+        let gridPixels = sketch.querySelectorAll('div');
+        gridPixels.forEach(gridPixel => gridPixel.remove());
+        createGrid(slider.value);
+    }
     
+    // On Page Load - default size
+    createGrid(16);
+
+    slider.addEventListener('mouseup', pixelSize);
+
     slider.addEventListener('input', () => {
         sliderValue.textContent = slider.value;
     });
-});
+
+    });
+
+
+
+/*
+
+    slider.addEventListener('mouseup', () => {
+        for (let j = 1; j <= slider.value; j++) {
+            let row = document.createElement("div");
+            row.setAttribute("class", "row");
+            sketch.appendChild(row);
+        
+            for (let i = 1; i <= slider.value; i++) {
+                let column = document.createElement("div");
+                column.setAttribute("class", "column");
+                row.appendChild(column);
+            };
+        };
+    });
+
+    function createGrid (gridNumber) { 
+        let gridArea = gridNumber * gridNumber;
+        for (let i = 1; i <= gridArea; i++) {
+            let gridItem = document.createElement('div');
+            sketch.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+            sketch.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
+            sketch.insertAdjacentElement('beforeend', gridItem);
+        } 
+        // let gridPixels = sketch.querySelectorAll('div');
+        // gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', colorGrid));
+    }
+
+    function pixelSize() {
+        let gridPixels = sketch.querySelectorAll('div');
+        gridPixels.forEach(gridPixel => gridPixel.remove());
+        createGrid(slider.value);
+    }
+
+*/
