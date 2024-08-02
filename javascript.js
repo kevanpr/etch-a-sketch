8859659c6bef26cc06e1a64e6a53cbbcc37e11a6
kevanpr <gracellepontanilla@gmail.com>
2024-08-02 23:21:50 +0800

    fix: add color randomizer for rainbow brush inside switch statement

--- javascript.js
+++ javascript.js
@@ -46,14 +46,28 @@ window.addEventListener("DOMContentLoaded", (event) => {
                 this.style.backgroundColor = "";
                 break;
             case "rainbow":
-                this.style.backgroundColor = rainbowBrush;
+                this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
                 break;
             default:
                 this.style.backgroundColor = "black";
         }
     }
-
-    
+      
+      function draw() {
+        colorMode(HSB);
+        // background(frameCount % 360, 100, 100);
+        if(mouseIsPressed){
+         noStroke();
+          stroke((5*frameCount) % 360, 40, 100);
+          fill((5*frameCount) % 360, 100, 100);
+      
+          // ellipse(mouseX, mouseY, 30,30);
+          strokeWeight(20);
+          line(mouseX, mouseY, pmouseX, pmouseY);
+        }
+        
+        colorMode(RGB);
+      }
 
     // rainbow brush
     function rainbowBrush () {
@@ -61,7 +75,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
         const hue = (frameCount * 10) % 360;
   
         // set the color and brush style
-        const hsbaColor = brushColor(`hsba(${hue}, 100%, 100%, 0.6)`);
+        const hsbaColor = (`hsba(${hue}, 100%, 100%, 0.6)`);
 
         return hsbaColor;
     }
